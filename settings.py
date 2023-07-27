84cd530764457beafd05994b405de519c89106f7
arduriki <jordi.ardura.professional@gmail.com>
2023-07-27 18:00:08 +0200

    Fire and deleting bullets

--- settings.py
+++ settings.py
@@ -16,5 +16,6 @@ class Settings:
         # Bullet settings
         self.bullet_speed = 2.0
         self.bullet_width = 3
-        self.bullet_heigh = 15
+        self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
